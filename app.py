aa8023584b2d82e480b570bbd483a6dc7f36601e
alfazick <nurbekoff@gmail.com>
2023-08-09 05:47:39 +0000

    uploaded only weights due to github limit

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
  
 from flask import Flask, render_template, request, url_for
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
 
 app = Flask(__name__, static_url_path='/images', static_folder='images')
 
 # Define the classes (adjust based on your model's classes)
-classes = ['Sakura', 'Jiraya', 'Sasuke']  # Replace with your class names
+classes = ['Jiraiya', 'Sakura', 'Sasuke']  # Replace with your class names
 
-# Load the saved model
-model_file_name = os.path.join('models', 'best_model.h5')
-model = load_model(model_file_name)
+# Recreate the model architecture
+base_model = ResNet50(weights=None, include_top=False)  # Set weights to None
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu')(x)  # Removed regularization for simplicity
+x = Dropout(0.6)(x)
+predictions = Dense(3, activation='softmax')(x)  # Adjusted for 3 classes
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Load the saved weights
+weights_file_name = os.path.join('models', 'model_weights_only.h5')
+model.load_weights(weights_file_name)
 
 @app.route('/')
 def home():
@@ -23,7 +35,7 @@ def classify_image():
     # Get the image from the POST request
     uploaded_image = request.files['image']
     img = load_img(uploaded_image, target_size=(224, 224))  # Adjust target size based on your model's input size
-    img_array = img_to_array(img)
+    img_array = img_to_array(img) / 255.  # Ensure to rescale as you did during training
     img_array = np.expand_dims(img_array, axis=0)
 
     # Use the model to predict the class
